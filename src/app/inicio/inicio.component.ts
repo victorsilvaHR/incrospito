7dd6448587152d43bb389cafc8cc52a56d55cb37
victorsilvaHR <victor.hugo.silva01@gmail.com>
2025-04-17 10:06:45 -0600

    se crea flecha que hace scroll

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
+  showButton: boolean = false; // Controla la visibilidad del botón
+
   constructor() { }
 
   ngOnInit(): void {
+    window.onscroll = () => {
+      // Mostrar el botón cuando el usuario haga scroll hacia abajo
+      this.showButton = (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200);
+    };
+    this.checkScroll();
+
+  }
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    // Mostrar el botón cuando el usuario haga scroll hacia abajo
+    this.showButton = (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200);
   }
 
+  scrollToTop(): void {
+    // Desplazarse hacia la parte superior de la página con animación
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
